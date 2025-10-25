0d29fb5d297990967564365a0fde1a6ace21c2b1
Keerti.Bannimatti <01fe24bca238@kletech.ac.in>
2025-10-25 18:49:42 +0530

    My first commit

---
+++ addsub.py
@@ -0,0 +1,13 @@
+
+num1_str = input("Enter the first number: ")
+num2_str = input("Enter the second number: ")
+
+num1 = float(num1_str)
+num2 = float(num2_str)
+
+sum_result = num1 + num2
+diff_result = num1 - num2
+
+# Display the results
+print(f"The sum of {num1} and {num2} is: {sum_result}")
+print(f"The diff of {num1} and {num2} is: {diff_result}")
