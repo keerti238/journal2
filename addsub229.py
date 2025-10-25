288615fbeb5d2086f82e12ae109dfc492cf96c05
Shweta.khanannavar <shwetamkhanannavar838@gmail.com>
2025-10-25 19:06:33 +0530

    Modified program

---
+++ addsub229.py
@@ -0,0 +1,21 @@
+num1_str = input("Enter the first number: ")
+num2_str = input("Enter the second number: ")
+num1 = float(num1_str)
+num2 = float(num2_str)
+# Perform addition
+sum_result = num1 + num2
+
+# Perform Subtraction
+difference= num1 - num2
+
+# Perform Multiplication
+product= num1 * num2
+
+# Perform Division
+division= num1/num2
+
+# Display the results
+print(f"The sum of {num1} and {num2} is: {sum_result}")
+print(f"The difference of {num1} and {num2} is: {difference}")
+print(f"The product of {num1} and {num2} is: {product}")
+print(f"The Division of {num1} and {num2} is: {division}")
